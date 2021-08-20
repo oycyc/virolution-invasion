8a4f310046759c5522a7e3cfcdf650f978075793
yangciou <me@yangciou.com>
2021-08-20 15:19:48 -0400

    resources falling from sky

--- js/resources.js
+++ js/resources.js
@@ -6,16 +6,25 @@ const amounts = [20, 30, 40];
 class Resource {
     constructor() {
         this.x = Math.random() * (constants.canvas.width - constants.cellSize);
+        // currentY is current position -- y is the final positional
+        this.currentY = 0;
         this.y = (Math.floor(Math.random() * 5) + 1) * constants.cellSize + 25;
         this.width = constants.cellSize * 0.6;
         this.height = constants.cellSize * 0.6;
         this.amount = amounts[Math.floor(Math.random() * amounts.length)];
+        this.speed = 5;
+    }
 
+    update() {
+        if (this.currentY < this.y) {
+            this.currentY += this.speed;
+
+        }
     }
 
     draw() {
         constants.ctx.fillStyle = "yellow";
-        constants.ctx.fillRect(this.x, this.y, this.width, this.height);
+        constants.ctx.fillRect(this.x, this.currentY, this.width, this.height);
         constants.ctx.fillStyle = "black";
         constants.ctx.font = "20px Arial";
         constants.ctx.fillText(this.amount, this.x + 15, this.y + 25);
@@ -28,6 +37,7 @@ export function handleResources() {
     }
 
     for (let i = 0; i < constants.resources.length; i++) {
+        constants.resources[i].update();
         constants.resources[i].draw();
         if (constants.resources[i] && constants.mouse.x && constants.mouse.y && collision(constants.resources[i], constants.mouse)) {
             constants.numberOfResources += constants.resources[i].amount;
