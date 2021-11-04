a9d488355d21919d6c656f4e20d0d30045b7fca2
yangciou <me@yangciou.com>
2021-11-04 11:32:08 -0400

    modals

--- js/script.js
+++ js/script.js
@@ -65,3 +65,42 @@ elements.startGameBtn.addEventListener("click", () => {
 
 
 
+/*********************************************
+  MODALS
+ *********************************************/
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach(item => {
+	item.addEventListener("click", () => {
+		const modal = document.querySelector(item.dataset.modalTarget);
+		openModal(modal);
+	})
+});
+
+closeModalButtons.forEach(item => {
+	item.addEventListener("click", () => {
+		const modal = item.closest(".modal");
+		closeModal(modal);
+	})
+});
+
+overlay.addEventListener("click", () => {
+	const modals = document.querySelectorAll(".modal.active");
+	modals.forEach(modal => {
+		closeModal(modal);
+	})
+});
+
+function openModal(modal) {
+	if (modal == null) return;
+	modal.classList.add("active");
+	overlay.classList.add("active");
+};
+
+function closeModal(modal) {
+	if (modal == null) return;
+	modal.classList.remove("active");
+	overlay.classList.remove("active");
+};
